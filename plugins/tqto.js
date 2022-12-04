a4701072930ec8ae4bd5aaece6dd4238f6d384a6
Denz4323 <dsaputr847@gmail.com>
2022-12-04 19:05:34 +0700

    name

---
+++ plugins/tqto.js
@@ -0,0 +1,62 @@
+import { createHash } from 'crypto'
+import fetch from 'node-fetch'
+
+let handler = async function (m, { text, usedPrefix, command }) {
+  const ultah = new Date(`${ultahowner} 00:00:01`)
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+let cap = `*BIG THANKS TO*
+    
+
+─────────────────────
+• God
+• My ortu
+• Orang Yang Bangun Script ini
+• Penyedia Layanan API
+─────────────────────
+Script ini direcode oleh ${nameown}
+`
+  let buttonMessage= {
+'document':{'url':`${syt}`},
+'mimetype':global.ddocx,
+'fileName':`「 ${namebot} 」`,
+'fileLength':fsizedoc,
+'pageCount':fpagedoc,
+'contextInfo':{
+'forwardingScore':555,
+'isForwarded':true,
+'externalAdReply':{
+'mediaUrl':`${syt}`,
+'mediaType':2,
+'previewType':'pdf',
+'title':`💌 Ultah Owner: ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+'body':`${namebot}`,
+'thumbnail':await(await fetch('https://i.ibb.co/CJPs6pH/20221203-095917.jpg')).buffer(),
+'sourceUrl':`${syt}`}},
+'caption':cap,
+'footer':`Ini Ucapan Terima Kasih Untuk Orang Yang Telah Berpatisipasi
+
+${botdate}`,
+'buttons':[
+{'buttonId':'.menu','buttonText':{'displayText':'MENU🎟️'},'type':1},
+{'buttonId':'.salken','buttonText':{'displayText':'HALLO👋'},'type':1}
+],
+'headerType':6}
+    await conn.sendMessage(m.chat,buttonMessage, { quoted:m})
+}
+handler.tags = ['xp']
+
+handler.command = /^(tqto)$/i
+
+export default handler
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
